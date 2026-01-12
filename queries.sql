9e82db0954b797d106602f5bade154ced29973ea
Eloi Dieme <eloidieme@gmail.com>
2026-01-12 14:19:38 +0000

    wip

--- queries.sql
+++ queries.sql
@@ -15,14 +15,36 @@ Pour les attributs des entités:
  - genres: noms
 */
 
--- Exercice 1: Année de naissance de l'artiste Jude Law
+-- Exercice 1: Visualisez l'année de naissance de l'artiste *Jude Law (Brad Pitt n'étant pas dans la BDD).
 SELECT primaryName, birthYear FROM tArtist WHERE primaryName LIKE '%Jude Law%';
-
 -- Commentaire: Jude Law est né en 1972. On le trouve à partir d'une simple requête sur une seule table avec filtre (WHERE .. LIKE).
 -- On utilise LIKE pour avoir plus de flexibilité sur le string-matching.
 
--- Exercice 2: Nombre d'artistes présents dans la BDD
+-- Exercice 2: Comptez le nombre d'artistes présents dans la base de données.
 SELECT COUNT(*) FROM tArtist;
-
 -- Commentaire: Il y a 82046 artistes dans la BDD. On utilise une fonction d'agrégation (COUNT) pour compter le nombre de lignes dans la table tArtist.
 
+-- Exercice 3: Trouvez les noms des artistes nés en 1960, affichez ensuite leur nombre.
+SELECT primaryName FROM tArtist WHERE birthYear = 1960;
+SELECT COUNT(*) FROM tArtist WHERE birthYear = 1960;
+-- Commentaire: Il y a 203 artistes nés en 1960 dans la BDD. On utilise ici aussi du filtrage et une fonction d'aggrégation.
+
+-- Exercice 4: Trouvez l'année de naissance la plus représentée parmi les acteurs (sauf 0!), et combien d'acteurs sont nés cette année là.
+SELECT birthYear, COUNT(*) FROM tArtist WHERE birthYear <> 0 GROUP BY birthYear ORDER BY COUNT(*) DESC;
+-- Commentaire: L'année de naissance la plus représentée parmi les acteurs est 1980 (avec 477 occurences). 
+-- Cette fois on combine une fonction d'aggrégation avec un GROUP BY pour grouper les artistes par année de naissance. 
+-- Enfin on ordonne pour obtenir le classement.
+
+-- Exercice 5: Trouvez les artistes ayant joué dans plus d'un film.
+SELECT a.primaryName, COUNT(j.idFilm) AS NumberOfFilms FROM tArtist AS a INNER JOIN tJob AS j ON a.idArtist = j.idArtist WHERE j.category = 'acted in' GROUP BY a.primaryName HAVING COUNT(j.idFilm) > 1;
+-- Commentaire: On utilise une jointure entre tArtist et tJob pour relier les artistes à leurs films. 
+-- Ensuite on groupe par artiste et on utilise HAVING pour filtrer ceux ayant plus d'un film.
+-- On filtre aussi sur la catégorie 'acted in' pour ne considérer que les rôles d'acteur.
+
+-- Exercice 6: Trouvez les artistes ayant eu plusieurs responsabilités au cours de leur carrière (acteur, directeur, producteur...).
+
+
+-- Exercice 7: Trouver le nom du ou des film(s) ayant le plus d'acteurs (i.e. uniquement acted in).
+
+
+-- Exercice 8: Montrez les artistes ayant eu plusieurs responsabilités dans un même film (ex: à la fois acteur et directeur, ou toute autre combinaison) et les titres de ces films.
